0357abf7124767e5276cd14c805206667f50eff3
mahmoud-ramadn <ahmedshawareb567@gmail.com>
2025-11-16 08:06:32 +0200

    signup  controller added

---
+++ backEnd/src/controllers/auth.controller.js
@@ -0,0 +1,59 @@
+import User from "../models/User.js";
+import bcrypt from "bcryptjs";
+import { generateToken } from "../lib/utils.js";
+export const signup = async (req, res) => {
+  const { fullName, email, password } = req.body;
+  try {
+    if (!fullName || !email || !password) {
+      return res.status(400).json({ error: "Please fill all fields" });
+    }
+
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ error: "Password must be at least 6 characters long" });
+    }
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+      return res.status(400).json({ error: "Invalid email address" });
+    }
+    const emailExists = await User.findOne({ email });
+    if (emailExists) {
+      return res.status(400).json({ error: "Email already exists" });
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      fullName,
+      email,
+      password: hashedPassword,
+    });
+    if (newUser) {
+      generateToken(newUser._id, res);
+      await newUser.save();
+      res.status(201).json({
+        message: "User created successfully",
+        data: {
+          _id: newUser._id,
+          fullName: newUser.fullName,
+          email: newUser.email,
+          profilePic: newUser.profilePic,
+        },
+      });
+    } else {
+      res.status(400).json({ error: "Failed to create user" });
+    }
+  } catch (error) {
+    console.error("Error signing up user:", error);
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+export const login = async (req, res) => {
+  res.send("Login EndPoint");
+};
+export const logout = async (req, res) => {
+  res.send("Logout EndPoint");
+};
--- backEnd/src/server.js
+++ backEnd/src/server.js
@@ -3,21 +3,25 @@ import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
 import path from "path";
+import { connectDB } from "./lib/db.js";
 dotenv.config();
 const app = express();
 const __dirname = path.resolve();
 const port = process.env.PORT || 3000;
 
+app.use(express.json());
+app.get("/", (req, res) => res.send({ message: "Welcome to the server" }));
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontEnd/dist")));
   app.get("*", (_, res) =>
-    res.sendFile(path.join(__dirname, "../frontEnd","dist","index.html"))
+    res.sendFile(path.join(__dirname, "../frontEnd", "dist", "index.html"))
   );
 }
 
 app.listen(port, () => {
   console.log("Server is running on port " + port);
+  connectDB();
 });
