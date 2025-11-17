99633803a3e5241c07e4dd0dcd37e6657901fa27
mahmoud-ramadn <ahmedshawareb567@gmail.com>
2025-11-17 17:50:34 +0200

    login& logout functionality added

--- backEnd/src/controllers/auth.controller.js
+++ backEnd/src/controllers/auth.controller.js
@@ -65,8 +65,40 @@ export const signup = async (req, res) => {
 };
 
 export const login = async (req, res) => {
-  res.send("Login EndPoint");
+  const { email, password } = req.body;
+
+  try {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+      return res.status(400).json({ message: "Invalid email address" });
+    }
+    const user = await User.findOne({ email });
+
+    if (!user) return res.status(404).json({ message: "User not found" });
+
+    const isPasswordCorrect = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordCorrect) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    generateToken(user._id, res);
+
+    res.status(201).json({
+      message: "Login successful",
+      data: {
+        _id: user._id,
+        fullName: user.fullName,
+        email: user.email,
+        profilePic: user.profilePic,
+      },
+    });
+  } catch (error) {
+    console.error("Error logging in user:", error);
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
 };
 export const logout = async (req, res) => {
-  res.send("Logout EndPoint");
+  res.cookie("jwt", "", { maxAge: 0 });
+  res.status(200).json({ message: "Logged out successfully" });
 };
