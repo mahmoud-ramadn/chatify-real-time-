02ecd3b3b5850c3e6b1c3df9e21f3737a78064eb
mahmoud-ramadn <ahmedshawareb567@gmail.com>
2025-11-18 21:51:32 +0200

    feat: add animated gradient border component and implement page loader
    - Created BorderAnimatedContainer component for animated gradient borders.

--- frontEnd/src/lib/axios.js
+++ frontEnd/src/lib/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const axiosInstance = axios.create({
   baseURL:
     import.meta.env.MODE === "development"
-      ? "http://localhost:3000/api"
+      ? "https://chatify-real-time-urcmv.sevalla.app"
       : "/api",
   withCredentials: true,
 });
