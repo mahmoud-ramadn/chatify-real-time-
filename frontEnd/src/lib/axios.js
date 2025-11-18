bb2dfe7d921308817a982e20bdf7a1a859abf8d3
mahmoud-ramadn <ahmedshawareb567@gmail.com>
2025-11-18 22:06:17 +0200

    fix:api url

--- frontEnd/src/lib/axios.js
+++ frontEnd/src/lib/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const axiosInstance = axios.create({
   baseURL:
     import.meta.env.MODE === "development"
-      ? "https://chatify-real-time-urcmv.sevalla.app"
+      ? "https://chatify-real-time-urcmv.sevalla.app/api"
       : "/api",
   withCredentials: true,
 });
