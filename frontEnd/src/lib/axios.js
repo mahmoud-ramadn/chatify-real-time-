77844e9fc0aa839543218167ce3656c3aea29954
mahmoud-ramadn <ahmedshawareb567@gmail.com>
2025-11-19 20:10:21 +0200

    fix: env fils

--- frontEnd/src/lib/axios.js
+++ frontEnd/src/lib/axios.js
@@ -4,6 +4,6 @@ export const axiosInstance = axios.create({
   baseURL:
     import.meta.env.MODE === "development"
       ? "http://localhost:3000/api"
-      : import.meta.env.VITE_CLIENT_URL,
+      : import.meta.env.VITE_API_URL,
   withCredentials: true,
 });
