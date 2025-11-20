4044918b2325aa7135a167fd67b37981053a392e
mahmoud-ramadn <ahmedshawareb567@gmail.com>
2025-11-20 15:23:36 +0200

    fisnh

--- frontEnd/src/store/useAuthStore.js
+++ frontEnd/src/store/useAuthStore.js
@@ -6,7 +6,7 @@ import { io } from "socket.io-client";
 const BASE_URL =
   import.meta.env.MODE === "development"
     ? "http://localhost:3000"
-    : "https://chatify-real-time.sevalla.app/api";
+    : "/api";
 
 export const useAuthStore = create((set, get) => ({
   authUser: null,
